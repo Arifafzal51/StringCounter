8e51171e4dbc075697b7711b55ddcb79f5db9d69
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-12-11 23:59:02 +0530

    Initial commit

---
+++ src/Counter.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Counter
+
+
+{
+    public static void main(String[] args) {
+        int i = 0;
+        String str;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String you want to count");
+        str=sc.nextLine();
+
+        int n=str.length();
+        System.out.println("length is= "+n);
+
+    }
+
+
+
+
+}
